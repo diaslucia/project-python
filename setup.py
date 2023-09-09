7c8e0489c4fe141627f66651c22361b930d16b38
diaslucia <luciadias.dev@gmail.com>
2023-09-09 17:14:54 -0300

    ADD configuration and setup folder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="project-python",
+    version= "1.0",
+    description= "",
+    author= "Lucia Dias",
+    author_email= "luciadias.dev@gmail.com",
+    packages= ["package"]
+)
