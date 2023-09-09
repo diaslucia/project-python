0d9b94c69663583ba35a805aff3d5cf7caa603e5
diaslucia <luciadias.dev@gmail.com>
2023-09-09 17:09:12 -0300

    fist commit

---
+++ menu.py
@@ -0,0 +1,5 @@
+from package.module1 import Persona
+
+juliana = Persona("juliana", "asdgmail.com")
+
+print(juliana)
---
+++ package/module1.py
@@ -0,0 +1,4 @@
+ class Persona:
+    def __init__(sefl, name, email):
+      self.name = name
+      self.email: email
